d4e64708db72d9d91d14d64542d3402f6e607654
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-10-06 19:11:47 -0300

    Criação da Seção eventos

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -22,8 +22,13 @@ export default function Home () {
                     </div>
                 </section>
 
-                <section>
-
+                <section className="secao-eventos">
+                    <div>
+                        <h2>NOSSOS EVENTOS</h2>
+                        <span>Delicias com sorvete!</span>
+                        <p>Mais do que uma sorveteria, uma extensão da sua casa! Estamos aqui prontinhos para te atender e oferecer os melhores eventos com os melhores sorvete da sua vida! Venha nos conhecer e passar um tempo aqui com a gente.</p>
+                    </div>
+                    <img src="./assets/eventos-image.jpg"/>
                 </section>
 
                 <section>
