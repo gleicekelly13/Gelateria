2de8711915fc5fa4025608a2889586f0ecafafde
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-10-06 19:18:44 -0300

    Criação da Seção Sobre

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -31,8 +31,13 @@ export default function Home () {
                     <img src="./assets/eventos-image.jpg"/>
                 </section>
 
-                <section>
-
+                <section className="secao-sobre">
+                    <img src="./assets/sobre-image.jpg"/>
+                    <div>
+                        <h2>SOBRE NÓS</h2>
+                        <span>Alegria em cada casquinha!</span>
+                        <p>Venha tomar o melhor sorvete da região aqui com a gente! Nós estamos há anos no mercado produzindo o que tem de melhor para o 	nosso cliente e você não pode ficar fora dessa. Venha nos fazer uma visita e aproveite o melhor atendimento e o melhor sorvete da cidade.</p>
+                    </div>
                 </section>
             </main>
         </div>
