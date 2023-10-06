0bb8f7d7d7a60f8285bf90045a17cfd5687a9238
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-10-06 19:05:46 -0300

    Criação da Seção Sabores

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -5,6 +5,31 @@ export default function Home () {
     return (
         <div>
             <Topo/>
+
+            <main>
+                <section className="secao-banner">
+                    <div>
+                        <h1>SORVETE ARTESANAL</h1>
+                    </div>
+                </section>
+
+                <section className="secao-sabores">
+                    <img src="./assets/banner-sabores.jpg" alt="image-sabores"/>
+                    <div>
+                        <h2>Nossos produtos</h2>
+                        <span>Novos e deliciosos!</span>
+                        <p>Sorvete bom é aquele feito com os melhores ingredientes! Aqui na gelateria todos os nossos produtos são naturais, à base de frutas e sem nenhum conservante! Também temos opções sem lactose e sem açúcar. Venha conhecer e perceber que tem como o sorvete ser delicioso e saudável ao mesmo tempo!</p>
+                    </div>
+                </section>
+
+                <section>
+
+                </section>
+
+                <section>
+
+                </section>
+            </main>
         </div>
     );
 }
