6a2a5f3d526a0e740880cf7678f32ff2b944696e
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-10-07 14:26:10 -0300

    Estilização do conteúdo principal

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -15,17 +15,17 @@ export default function Home () {
                     </div>
                 </section>
 
-                <section className="secao-sabores">
+                <section className="secao-sabores secao-itens">
                     <img src="./assets/banner-sabores.jpg" alt="image-sabores"/>
-                    <div>
-                        <h2>Nossos produtos</h2>
+                    <div className="secao-conteudo">
+                        <h2>Nossos sabores</h2>
                         <span>Novos e deliciosos!</span>
                         <p>Sorvete bom é aquele feito com os melhores ingredientes! Aqui na gelateria todos os nossos produtos são naturais, à base de frutas e sem nenhum conservante! Também temos opções sem lactose e sem açúcar. Venha conhecer e perceber que tem como o sorvete ser delicioso e saudável ao mesmo tempo!</p>
                     </div>
                 </section>
 
-                <section className="secao-eventos">
-                    <div>
+                <section className="secao-eventos secao-itens">
+                    <div className="secao-conteudo">
                         <h2>NOSSOS EVENTOS</h2>
                         <span>Delicias com sorvete!</span>
                         <p>Mais do que uma sorveteria, uma extensão da sua casa! Estamos aqui prontinhos para te atender e oferecer os melhores eventos com os melhores sorvete da sua vida! Venha nos conhecer e passar um tempo aqui com a gente.</p>
@@ -33,9 +33,9 @@ export default function Home () {
                     <img src="./assets/eventos-image.jpg"/>
                 </section>
 
-                <section className="secao-sobre">
+                <section className="secao-sobre secao-itens">
                     <img src="./assets/sobre-image.jpg"/>
-                    <div>
+                    <div className="secao-conteudo">
                         <h2>SOBRE NÓS</h2>
                         <span>Alegria em cada casquinha!</span>
                         <p>Venha tomar o melhor sorvete da região aqui com a gente! Nós estamos há anos no mercado produzindo o que tem de melhor para o 	nosso cliente e você não pode ficar fora dessa. Venha nos fazer uma visita e aproveite o melhor atendimento e o melhor sorvete da cidade.</p>
