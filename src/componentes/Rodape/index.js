bbdf798a23b0d036c2346e91fdad47fe24218a98
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-10-07 16:30:04 -0300

    Estilização do rodapé

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -3,28 +3,28 @@ import './style.css';
 export default function Rodape () {
     return (
         <footer>
-            <div>
+            <div className="limita-secao container-cards">
                 <img src="./assets/logo.png" alt="image-logo"/>
                 <div>
-                    <h4>ENDEREÇO</h4>
+                    <h3>ENDEREÇO</h3>
                     <p>Av. Bernardino de Campos, 98</p>
                     <p>São Paulo, SP 12345-678</p>
                 </div>
 
                 <div>
-                    <h4>CONTATO</h4>
+                    <h3>CONTATO</h3>
                     <p>info@meusite.com</p>
                     <p>Tel: (11) 3456-7890</p>
                 </div>
 
                 <div>
-                    <h4>HORÁRIOS</h4>
+                    <h3>HORÁRIOS</h3>
                     <p>ABERTO TODOS OS DIAS</p>
                     <p>10:00 - 22:00</p>
                 </div>
             </div>
 
-            <div>
+            <div className='limitar-secao rodape2'>
                 <p>Gelateria. Orgulhosamente desenvolvido por Gleice Kelly</p>
             </div>
         </footer>
