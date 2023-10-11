c3d09577ddf05a4a1592b2633324d517453717d1
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-10-10 21:04:49 -0300

    Criação da página Sobre

--- src/paginas/Sobre/index.js
+++ src/paginas/Sobre/index.js
@@ -1,10 +1,41 @@
 import React from "react";
+import Topo from "../../componentes/Topo";
 import './style.css';
+import Rodape from "../../componentes/Rodape";
 
 export default function Sobre () {
     return (
         <div>
-            Sobre
+            <Topo/>
+
+            <main>
+                <section className="secao-banner-sobre">
+                    <div className="texto-sobre">
+                        <h1>A GELATERIA</h1>
+                    </div>
+                </section>
+
+                <section className="secao-textos-sobre">
+                    <div>
+                        <h2>Sobre Nós</h2>
+                        <span>Nós simplesmente amamos sorvete!</span>
+                        <p>Somos uma empresa apaixonada pelo que faz. Colocamos amor em cada sorvete produzido. Fazemos o melhor sorvete para os nossos clientes e gostamos de receber elogios. Estamos operando desde 2009 com as melhores matérias-primas para a produção final do sorvete. Vendemos tanto para varejo como para atacado.</p>
+                        <p>Nossos clientes podem comprar os nossos sorvetes e degustar na nossa loja ou levar para sua residência e aproveitar junto com a família. Também vendemos para estabelecimentos e criamos eventos como festa de aniversário, formaturas e eventos empresariais. Para contratar os nossos serviços, basta entrar em contato conosco. Iremos proporcionar o melhor atendimento e os melhores produtos para você fazer a sua festa mais saborosa, com o melhor sorvete da cidade.</p>
+                    </div>
+                </section>
+
+                <section className="secao-images-sobre">
+                    <div>
+                        <img src="./assets/sobre-image.jpg"/>
+                    </div>
+                    <div>
+                        <img src="./assets/sorveteria.jpg"/>
+                    </div>
+                    
+                </section>
+            </main>
+
+            <Rodape/>
         </div>
     );
 }
